0b570f33b1470d1cc2665f1d94e36fd4a48a05aa
Pedro <pedroberorato@gmail.com>
2020-09-25 15:36:07 -0300

    Isolando Components

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -21,45 +21,11 @@ export const Content = styled.div`
 
   form {
     margin: 50px 0;
-    width: 300px;
+    width: 340px;
     text-align: center;
 
     h1 {
       margin-bottom: 24px;
-    }
-
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      color: #f4ede8;
-      padding: 16px;
-      width: 100%;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      border-radius: 10px;
-      border: 0;
-      color: #313238;
-      font-weight: 500;
-      height: 56px;
-      margin-top: 16px;
-      padding: 0 16px;
-      width: 100%;
-      transition: background 0.3s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
     }
 
     a {
