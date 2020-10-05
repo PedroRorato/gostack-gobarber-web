b9cd81cc341ce6a8d2b879520fd067413d19d7ab
Pedro <pedroberorato@gmail.com>
2020-10-05 15:24:25 -0300

    Login pelo Context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Pedro' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
+
     <GlobalStyle />
   </>
 );
